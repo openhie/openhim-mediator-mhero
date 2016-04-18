b7e58bca2f29296ef02146ba476d469fb3ff5c7e
Ryan Crichton <rg.crichton@gmail.com>
2016-04-18 10:52:20 +0200

    MHER-35 Add function to convert CSD to a Contact

--- utils.js
+++ utils.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const Dom = require('xmldom').DOMParser
+const xpath = require('xpath')
+
 exports.buildOrchestration = (name, beforeTimestamp, res, body) => {
   return {
     name: name,
@@ -13,5 +16,25 @@ exports.buildOrchestration = (name, beforeTimestamp, res, body) => {
       body: body,
       timestamp: new Date()
     }
+  }
+}
+
+exports.convertCSDToContact = (entity) => {
+  const doc = new Dom().parseFromString(entity)
+  const select = xpath.useNamespaces({'csd': 'urn:ihe:iti:csd:2013'})
+  const name = select('/csd:provider/csd:demographic/csd:name/csd:commonName/text()', doc).toString()
+  const telNodes = select('/csd:provider/csd:demographic/csd:contactPoint/csd:codedType[@code="BP" and @codingScheme="urn:ihe:iti:csd:2013:contactPoint"]/text()', doc)
+  let tels = []
+  telNodes.forEach((telNode) => {
+    tels.push('tel:' + telNode.toString())
+  })
+
+  if (tels.length === 0) {
+    throw new Error('couldn\'t find a telephone number, this is a required field for a contact')
+  }
+
+  return {
+    name: name,
+    urns: tels
   }
 }
