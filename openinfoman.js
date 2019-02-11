798099861597811fa11f1b30c011b604d3a85979
Ally Shaban <allyshaban5@gmail.com>
2019-02-11 11:32:26 +0300

    adding auth support

--- openinfoman.js
+++ openinfoman.js
@@ -10,27 +10,30 @@ module.exports = function (cnf) {
 
   return {
     /**
-    * fetchAllEntities - fetches all entities in a particular CSD document and
-    * callsback with the full CSD document.
-    *
-    * @param {Function} callback The callback takes the form of
-    * callback(err, result, orchestrations).
-    */
-    fetchAllEntities: function (last_sync,reset,callback) {
+     * fetchAllEntities - fetches all entities in a particular CSD document and
+     * callsback with the full CSD document.
+     *
+     * @param {Function} callback The callback takes the form of
+     * callback(err, result, orchestrations).
+     */
+    fetchAllEntities: function (last_sync, reset, callback) {
       let uri = new URI(config.url)
         .segment('/CSD/csr/')
         .segment(config.queryDocument)
         .segment('careServicesRequest')
         .segment('/urn:ihe:iti:csd:2014:stored-function:provider-search')
 
-      if(reset) {
+      if (reset) {
         var record = '<csd:record updated="1970-01-01T00:00:00"/>'
-      }
-      else
-        var record = '<csd:record updated="' + last_sync +'"/>'
+      } else
+        var record = '<csd:record updated="' + last_sync + '"/>'
+      var username = config.username
+      var password = config.password
+      var auth = "Basic " + new Buffer(username + ":" + password).toString("base64")
       var options = {
         url: uri.toString(),
         headers: {
+          Authorization: auth,
           'Content-Type': 'text/xml'
         },
         body: `<csd:requestParams xmlns:csd="urn:ihe:iti:csd:2013">
@@ -56,6 +59,9 @@ module.exports = function (cnf) {
      */
     loadProviderDirectory: function (providers, callback) {
       let orchestrations = []
+      var username = config.username
+      var password = config.password
+      var auth = "Basic " + new Buffer(username + ":" + password).toString("base64")
 
       let emptyDirectoryURI = new URI(config.url)
         .segment('/CSD/emptyDirectory/')
@@ -77,6 +83,7 @@ module.exports = function (cnf) {
         var options = {
           url: updateURI.toString(),
           headers: {
+            Authorization: auth,
             'Content-Type': 'text/xml'
           },
           body: `<requestParams xmlns="urn:ihe:iti:csd:2013" xmlns:csd="urn:ihe:iti:csd:2013">
